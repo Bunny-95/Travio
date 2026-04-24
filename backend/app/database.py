38d57004c28e21ace2a683a8be8ad297ac175fc1
Srujan <srujanl867@gmail.com>
2026-04-24 18:48:56 +0530

    database conected

--- backend/app/database.py
+++ backend/app/database.py
@@ -7,8 +7,17 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL not found")
+
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)
+
 Base = declarative_base()
 
 def get_db():
